86f89b48d7195ae8fc9fdbcc81f27b907788e05b
Muhammad Abdi <abdi@unictive.net>
2021-10-18 19:23:41 +0700

    update translation

--- resources/lang/en/otp.php
+++ resources/lang/en/otp.php
@@ -3,5 +3,5 @@
 return [
     'invalid' => 'The :attribute is invalid.',
     'expired' => 'The :attribute has expired.',
-    'throttle' => 'Too many regenerate otp attempts. Please try again in :minutes minutes.',
+    'throttle' => 'Too many request extra time attempts. Please try again in :minutes minutes.',
 ];
